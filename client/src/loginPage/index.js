dbec357a87d153caa7f9921b74aa3b9398528fdd
Steven Ding <anluoridge@gmail.com>
2018-07-30 15:03:30 +1000

    add login test func in React move register func to Register Page

--- client/src/loginPage/index.js
+++ client/src/loginPage/index.js
@@ -1,28 +1,30 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+const axios = require('axios')
 
-// import {RegisteAccount} from './actions'
 export default () => {
     return (
         <div>
             <h1>Welcome</h1>
             <div><p>username</p><input type="text"/></div>
             <div><p>password</p><input type="text" /></div>
-            <button onClick={() => { 
-                axios({
-                    method: 'post',
-                    url: 'http://localhost:5000/api/users/register',
-                    header: {
-                        'Access-Control-Allow-Origin': '*',
-                        'content-type': 'application/x-www-form-urlencoded',
-                    },
-                    data: {
-                        name: '165468sd',
-                        email: 'dasihdoiahsdo@hotmail.com',
-                        password: 'dasdaasdasdasdssdasdasdas',
-                    }
-                });
-             }}>Submmit</button>
+          <button onClick={() => {
+
+            axios({
+              method: 'post',
+              url: 'http://localhost:5000/api/users/login',
+              header: {
+                'Access-Control-Allow-Origin': '*',
+                'content-type': 'application/x-www-form-urlencoded',
+              },
+              data: {
+                email: 'dasihdoiahsdo@hotmail.com',
+                password: 'dasdaasdasdasdssdasdasdas',
+              }
+            }
+            ).then(function (response) {
+                console.log(response);
+              });
+          }}>Login</button>
         </div>
     )
 }
