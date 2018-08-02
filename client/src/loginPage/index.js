ea9630363c2a8a81062d24b5ea8f09353cb2bb65
Steven Ding <anluoridge@gmail.com>
2018-08-02 18:49:46 +1000

    refactor the form in login page to React style

--- client/src/loginPage/index.js
+++ client/src/loginPage/index.js
@@ -1,28 +1,56 @@
 import React, { Component } from 'react';
-const axios = require('axios')
+const axios = require('axios');
 
-export default () => {
+class LoginForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      email: e.target.email,
+      password: e.target.password
+    })
+  }
+
+  handleSubmit(e) {
+    alert('submitted' + this.state.email + '\n' + this.state.password);
+    axios({
+        method: 'post',
+        url: 'http://localhost:5000/api/users/login',
+        header: {
+          'Access-Control-Allow-Origin': '*',
+          'content-type': 'application/x-www-form-urlencoded',
+        },
+        data: {
+          email: 'dasihdoiahsdo@hotmail.com',
+          password: 'dasdaasdasdasdssdasdasdas',
+        }
+      }
+    ).then(function (response) {
+      console.log(response);
+      console.log(response.data)
+    });
+    e.preventDefault()
+  }
+
+  render() {
     return (
-        <div>
-            <h1>Welcome</h1>
-            <div><p>username</p><input type="text"/></div>
-            <div><p>password</p><input type="text" /></div>
-          <button onClick={() => {
-            axios({
-              method: 'post',
-              url: 'http://localhost:5000/api/users/login',
-              header: {
-                'content-type': 'application/x-www-form-urlencoded',
-              },
-              data: {
-                email: 'dasihdoiahsdo@hotmail.com',
-                password: 'dasdaasdasdasdssdasdasdas',
-              }
-            }
-            ).then(function (response) {
-                console.log(response);
-              });
-          }}>Login</button>
-        </div>
+      <form onSubmit={this.handleSubmit}>
+        <h1>Welcome</h1>
+        <label>username<input type="email" value={this.state.email} onChange={this.handleChange}/></label>
+        <div><p>password</p><input type="password"/></div>
+        <input type="submit" value="submit"/>
+      </form>
     )
+  }
 }
+
+export default LoginForm
