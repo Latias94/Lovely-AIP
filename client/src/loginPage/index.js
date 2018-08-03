5e49651a97706d4ff3601363840b4cbf0de63526
Steven Ding <anluoridge@gmail.com>
2018-08-03 19:10:21 +1000

    Login page (#1)
    * refactor the form in login page to React style
    
    * get the right state of each input field.
    
    * fix data source in POST.
    add onChange to password.
    
    * remove ReactDOM
    replace export default
    
    * add login with Google button and forgot password option
    clean code

--- client/src/loginPage/index.js
+++ client/src/loginPage/index.js
@@ -1,28 +1,91 @@
 import React, { Component } from 'react';
-const axios = require('axios')
+const axios = require('axios');
 
-export default () => {
-    return (
-        <div>
-            <h1>Welcome</h1>
-            <div><p>username</p><input type="text"/></div>
-            <div><p>password</p><input type="text" /></div>
-          <button onClick={() => {
-            axios({
-              method: 'post',
-              url: 'http://localhost:5000/api/users/login',
-              header: {
-                'content-type': 'application/x-www-form-urlencoded',
-              },
-              data: {
-                email: 'dasihdoiahsdo@hotmail.com',
-                password: 'dasdaasdasdasdssdasdasdas',
-              }
-            }
-            ).then(function (response) {
-                console.log(response);
-              });
-          }}>Login</button>
-        </div>
+export default class LoginForm extends Component {
+  constructor(props) {
+    super(props);
+
+    // init state
+    this.state = {
+      email: '',
+      password: ''
+    };
+
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleEmailChange(e) {
+    this.setState({
+      email: e.target.value
+    })
+  }
+
+  handlePasswordChange(e) {
+    this.setState({
+      password: e.target.value
+    })
+  }
+
+  handleSubmit(e) {
+    axios({
+        method: 'post',
+        url: 'http://localhost:5000/api/users/login',
+        header: {
+          'Access-Control-Allow-Origin': '*',
+          'content-type': 'application/x-www-form-urlencoded',
+        },
+        data: {
+          email: this.state.email,
+          password: this.state.password,
+        }
+      }
+    ).then(function (response) {
+      console.log(response);
+      console.log(response.data)
+    });
+    e.preventDefault()
+  }
+
+
+render() {
+  const { forgotPasswordStyle, buttonStyle } = styles
+  return (
+      <div>
+      <form onSubmit={this.handleSubmit}>
+        <h1>Log in</h1>
+        <label>Username (Email)<input type="email" value={this.state.email} onChange={this.handleEmailChange}/></label>
+        <br/>
+        <label>Password<input type="password" value={this.state.password} onChange={this.handlePasswordChange}/></label>
+        <br/>
+        <input type="submit" value="Sign in"/>
+      </form>
+      <br/>
+      <button type="button" style={buttonStyle}>Log in with your Google account</button>
+        <br/>
+        <a href="http://www.w3school.com.cn" style={forgotPasswordStyle}>Forgot password?</a>
+    {/*TODO: 组件库*/}
+    {/*<Button bsStyle="success">Log in with your Google account</Button>*/}
+      </div>
     )
+  }
+}
+
+
+const styles = {
+  forgotPasswordStyle: {
+    color: 'gray',
+    textDecoration: 'underline'
+  },
+  buttonStyle: {
+    flex: 1,// extend as much as it can
+    alignSelf: 'stretch',
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: '#007aff',
+    marginLeft: 5,
+    marginRight: 5
+  },
 }
