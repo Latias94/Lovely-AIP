d62513be909becd021d0b32158d0edaefbad0909
Steven Ding <anluoridge@gmail.com>
2018-08-05 10:46:07 +1000

    create account page

---
+++ client/src/accountPage/index.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+const axios = require('axios');
+
+export default class accountPage extends Component {
+
+  // init state
+  // state = {
+  //   username: '',
+  //   email: '',
+  //   password: ''
+  // }
+
+  render() {
+    const {} = styles
+
+    return <div></div>
+  }
+}
+
+const styles = {
+
+}
