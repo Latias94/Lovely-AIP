f6e9b0200a05ae3683e750c939da97ababe98d6b
Steven Ding <anluoridge@gmail.com>
2018-08-10 09:54:23 +1000

    ready to rename branch add enzyme

--- client/src/registerPage/register.test.js
+++ client/src/registerPage/register.test.js
@@ -1,7 +1,5 @@
-import FormHelperText from '@material-ui/core/FormHelperText';
-import React from 'react';
-
 const puppeteer = require('puppeteer');
+import { mount } from 'enzyme';
 
 let browser;
 let page;
@@ -49,7 +47,7 @@ describe('Test validation of sign up =', () => {
   it('Length of password > 5', async () => {
 
 		const browser = await puppeteer.launch({
-			headless: false,
+			headless: false
 		});
 		const page = await browser.newPage();
 		await page.setViewport({ width: 1280, height: 800 });
@@ -62,6 +60,54 @@ describe('Test validation of sign up =', () => {
 
 		const errorMsg = await page.$eval('#password-helper-text', e => e.innerHTML);
 		expect(errorMsg).toBe('Password must be at least 6 character.');
+
+    await page.screenshot({
+      path: `./__tests__/screenshots/register-${Date.now()}.png`,
+      fullPage: true,
+    });
+
 		await browser.close();
 	});
+
+  it('Button click calls onAdd', () => {
+    onAdd = jest.fn();
+    add = mount(<Add onAdd={onAdd} />);
+    const button = add.find('button').first();
+    const input = add.find('input').first();
+    input.simulate('change', { target: { value: 'Name 4' }});
+    button.simulate('click');
+    expect(onAdd).toBeCalledWith('Name 4');
+  });
 });
+
+
+// "jest": {
+//   "collectCoverageFrom": [
+//     "src/**/*.{js,jsx}"
+//   ],
+//     "setupFiles": [
+//     "<rootDir>/config/polyfills.js"
+//   ],
+//     "testMatch": [
+//     "<rootDir>/src/**/__tests__/**/*.js?(x)",
+//     "<rootDir>/src/**/?(*.)(spec|test).js?(x)"
+//   ],
+//     "testEnvironment": "node",
+//     "testURL": "http://localhost",
+//     "transform": {
+//     "^.+\\.(js|jsx)$": "<rootDir>/node_modules/babel-jest",
+//       "^.+\\.(scss|css)$": "<rootDir>/config/jest/cssTransform.js",
+//       "^(?!.*\\.(js|jsx|css|scss|json)$)": "<rootDir>/config/jest/fileTransform.js"
+//   },
+//   "transformIgnorePatterns": [
+//     "[/\\\\]node_modules[/\\\\].+\\.(js|jsx)$"
+//   ],
+//     "moduleFileExtensions": [
+//     "web.js",
+//     "js",
+//     "json",
+//     "web.jsx",
+//     "jsx",
+//     "node"
+//   ]
+// }
