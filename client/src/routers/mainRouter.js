c273a63f423fb69a991f0a3fc818f58680208a08
Steven Ding <anluoridge@gmail.com>
2018-08-11 08:51:32 +1000

    add store in src add Provider in mainRouter

--- client/src/routers/mainRouter.js
+++ client/src/routers/mainRouter.js
@@ -1,24 +1,28 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
+import { Provider } from 'react-redux';
 import Welcome from '../welcomePage';
 import RegisterForm from '../registerPage';
 import LoginForm from '../loginForm';
 import BooksPage from '../booksPage';
 import AccountPage from '../accountPage/AccountPage';
+import store from '../store';
 
 const MainRoute = () => (
-	<Router>
-		<Route>
-			<Switch>
-				<Route exact path="/" component={Welcome} />
-				<Route path="/register" component={RegisterForm} />
-				<Route path="/login" component={LoginForm} />
-				<Route path={'/account'} component={AccountPage} />
-				<Route path="/book/:id" component={BooksPage}/>
-			</Switch>
-		</Route>
-	</Router>
+	<Provider store={ store }>
+		<Router>
+			<Route>
+				<Switch>
+					<Route exact path="/" component={Welcome} />
+					<Route path="/register" component={RegisterForm} />
+					<Route path="/login" component={LoginForm} />
+					<Route path={'/account'} component={AccountPage} />
+					<Route path="/book/:id" component={BooksPage}/>
+				</Switch>
+			</Route>
+		</Router>
+	</Provider>
 );
 
 export default MainRoute;
