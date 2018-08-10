70ea5cbfa0ac85a8b359951c2c5a51c717743e8a
Steven Ding <anluoridge@gmail.com>
2018-08-10 12:09:39 +1000

    Auto stash before revert of "ready to rename branch"

--- client/src/registerPage/register.test.js
+++ client/src/registerPage/register.test.js
@@ -3,35 +3,90 @@ import React from 'react';
 
 const puppeteer = require('puppeteer');
 
-describe('Test validation of sign up =', () => {
-	test('Length of password > 5', async () => {
-    beforeEach(function() {
-      originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
-      jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
-    });
+describe('Test account page', () => {
+	test('App name === Knight Frank', async () => {
+		const browser = await puppeteer.launch({
+			headless: false,
+		});
 
-    afterEach(function() {
-      jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
-    });
+		const page = await browser.newPage();
+		await page.setViewport({ width: 1280, height: 800 });
 
+		await page.goto('localhost:3000/login', { waitUntil: 'networkidle2' });
+		// TODO: move to App.test.js
+		const title = await page.title();
+		expect(title).toBe('Knight Frank'); // code behind it won't execute
+		await browser.close();
+	});
+
+	test('view account page', async () => {
 		const browser = await puppeteer.launch({
 			headless: false,
 		});
 		const page = await browser.newPage();
 		await page.setViewport({ width: 1280, height: 800 });
-		await page.goto('localhost:3000/register', { waitUntil: 'networkidle2' });
-		await page.waitFor(2000);
-    jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
-    // await page.focus('#username');
-		await page.focus('input[name=email]');
-		// await page.type('test@mail.com');
-		// await page.focus('input[name="username"]');
-		await page.type('username');
+
+		// page.emulate({
+		//   viewport: {
+		//     width: 500,
+		//     height: 2400,
+		//   },
+		//   userAgent: ''
+		// })
+
+		//   page.on('dialog', async dialog => {
+		//     console.log(dialog.message());
+		//     await dialog.dismiss();
+		// });
+		// await page.authenticate({ username: 'admin', password: 'admin' });
+		await page.goto('localhost:3000/login', { waitUntil: 'networkidle2' });
+
+		//   const html = await page.$eval('.App-title', e => e.innerHTML);
+		//   expect(html).toBe('Welcome to React');
+
+
+		//   test('Title == Pronto Tools', async () => {
+		//     await page.goto(url);
+		//     const title = await page.title();
+		//     expect(title).toBe("Pronto Tools");
+		//   });
+		//   test("Center == Tools for Your Growing Business", async () => {
+		//     await page.goto(url);
+		//     const center = await page.$eval('h2.font-34.uppercase > strong', e => e.innerHTML);
+		//     expect(center).toBe("Tools for Your Growing Business");
+		//   });
+
+
+		//   await page.goto('localhost:3000/login', {waitUntil: 'networkidle2'});
+		//   await page.goto('http://192.168.1.254/lancfg2menu.cgi?brName=Default', {waitUntil: 'networkidle2'});
+
+
+		//   await page.waitFor('input[name=email]');
+		//   await page.waitForSelector('#email');
+
+		//   await page.click('input[type="submit"]');
+		await page.focus('#email');
+		// for (let i = 0; i< '192.168.1.254'.length; i++) {
+		//     await page.keyboard.press('Delete');
+		// }
+		await page.keyboard.type('sf4@sf.com');
 		await page.focus('input[name="password"]');
-		await page.type('short');
-		await page.focus('input[name="password2"]');
-		await page.type('short');
-		await page.click('input[type="submit"]');
+		await page.keyboard.type('12345678');
+		//   await page.focus('input[name="chkIgmpSnp"]')
+		//   await page.type('#login_field', 'admin')
+		await page.screenshot({
+			path: '/Users/anluoridge/Downloads/TEMP/KnightFrank.png',
+			fullPage: true,
+		});
+		const inputElement = await page.$('input[type=submit]');
+		await inputElement.click();
+		await page.waitFor(1000);
+		// 等待几秒
+		// _sleep(5);
+		//   await page.goto('localhost:3000/account'); // a new tab
+		const accountButton = await page.$('a[href$=account]');
+		await accountButton.click();
+		await page.waitFor(3000);
 
 		const errorMsg = await page.$eval('#password-helper-text', e => e.innerHTML);
 		expect(errorMsg).toBe('Password must be at least 6 character.');
