d3e5d76ffd016abe1564e419599378a44e716417
Steven Ding <anluoridge@gmail.com>
2018-08-23 11:34:48 +1000

    adding ESDoc to email verification page

--- client/src/account/EmailVerification/index.js
+++ client/src/account/EmailVerification/index.js
@@ -2,22 +2,18 @@ import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
 import { styles } from '../AccountStyles';
 
-function jumpToUsersMailbox() {
-	// route to new page by changing window.location
-	window.open('http://mail.google.com', '_blank');
-}
+/**
+ * Email verification page
+ * @todo use Button property for onClick
+ * @todo countDown()
+ * @todo complete func sendActivationEmail()
+ * @todo jumpToUsersMailbox() should support various mailbox
+ */
 
-// TODO
-function countDown() {
-	return '(60s)';
-}
-
-
-export default class EmailVerification extends Component {
+class EmailVerification extends Component {
 
 	sendActivationEmail = email => {
-alert("Sorry, this feature is under development.")
-		// TODO: api /active/
+		alert("Sorry, this feature is under development.")
 	};
 
 	render() {
@@ -26,13 +22,21 @@ alert("Sorry, this feature is under development.")
 		return (
 			<div style={container}>
 				<h2>Thank you for signing up!</h2>
-				<p> We have sent an email with an activation link to your email address. In order to complete the sign-up process, please click the activation link. If you didn't receive the activation email, click on the button below to resend it.</p>
-				{/* TODO: button property for onClick
-					TODO: Gmail for now */}
+				<p> We have sent an email with an activation link to your email address. In order to complete the sign-up process, please click the activation link.
+					If you didn't receive the activation email, click on the button below to resend it.</p>
 				<div style={{ padding: '20px' }} onClick={jumpToUsersMailbox}><Button variant="contained" color="secondary" >Go to your mail box</Button></div>
-				{/* TODO: */}
-				<div onClick={this.sendActivationEmail}><Button variant="contained">Resend the verification email{countDown()}</Button></div>
+				<div onClick={this.sendActivationEmail}><Button variant="contained">Resend the verification email {countDown()}</Button></div>
 			</div>
 		);
 	}
 }
+
+function jumpToUsersMailbox() {
+  window.open('http://mail.google.com', '_blank');
+}
+
+function countDown() {
+  return '(60s)';
+}
+
+export default EmailVerification;
