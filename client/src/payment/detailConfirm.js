30d9eb73788080b211339b350e7ccc2bdcaa617c
Unknown <jennyhu19900914@gmail.com>
2018-08-24 19:33:48 +1000

    Add interface of Payment

--- client/src/payment/detailConfirm.js
+++ client/src/payment/detailConfirm.js
@@ -1,27 +1,72 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+
+const styles = theme => ({
+    root: {
+        width: '80%',
+        marginTop: theme.spacing.unit * 3,
+        overflowX: 'auto',
+    },
+    table: {
+        minWidth: 700,
+    },
+});
+
+let id = 0;
+function createData(BookName, Price, Amount, Cost ) {
+    id += 1;
+    return { id, BookName, Price,Amount, Cost};
+}
+
+const rows = [
+    createData('Big Data', 32.1, 2, 64.2),
+    createData('Data Visulation', 29.1, 1, 29.1),
+    createData('Internet Programming', 10, 3, 30),
+];
+
 
 class DetailConfirm extends Component{
     render(){
+        const { classes } = this.props;
         return(
-            <table>
-                <tr>
-                    <th>Book Name</th>
-                    <th>Price($)</th>
-                    <th>Amount</th>
-                    <th>Total</th>
-                </tr>
-                <tr>
-                    <td>Big Data</td>
-                    <th>123</th>
-                    <th>2</th>
-                    <th>246</th>
-                </tr>
-            </table>
-
+                <Table className={classes.table}>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Book Name</TableCell>
+                            <TableCell numeric>Price(AU$)</TableCell>
+                            <TableCell numeric>Amount</TableCell>
+                            <TableCell numeric>Cost</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {rows.map(row => {
+                            return (
+                                <TableRow key={row.id}>
+                                    <TableCell component="th" scope="row">
+                                        {row.BookName}
+                                    </TableCell>
+                                    <TableCell numeric>{row.Price}</TableCell>
+                                    <TableCell numeric>{row.Amount}</TableCell>
+                                    <TableCell numeric>{row.Cost}</TableCell>
+                                </TableRow>
+                            );
+                        })}
+                    </TableBody>
+                </Table>
         );
 
     }
 
 }
 
-export default DetailConfirm;
+DetailConfirm.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(DetailConfirm);
--- client/src/payment/index.js
+++ client/src/payment/index.js
@@ -2,66 +2,241 @@ import React from 'react';
 import './payment.css'
 import DetailConfirm from './detailConfirm';
 import DeliveryInfor from './deliveryInfor';
-import PaymentConfirm from './paymentConfirm';
+import PaymentSelect from './paymentSelect';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
+import Stepper from '@material-ui/core/Stepper';
+import Step from '@material-ui/core/Step';
+import StepButton from '@material-ui/core/StepButton';
+import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-
-
-function TabContainer(props) {
-    return (
-        <Typography component="div" style={{ padding: 8 * 3 }}>
-            {props.children}
-        </Typography>
-    );
-}
-
-TabContainer.propTypes = {
-    children: PropTypes.node.isRequired,
-};
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
 
 const styles = theme => ({
     root: {
-        flexGrow: 1,
-        backgroundColor: theme.palette.background.paper,
+        width: '90%',
+    },
+    button: {
+        marginRight: theme.spacing.unit,
+    },
+    backButton: {
+        marginRight: theme.spacing.unit,
+    },
+    completed: {
+        display: 'inline-block',
+    },
+    instructions: {
+        marginTop: theme.spacing.unit,
+        marginBottom: theme.spacing.unit,
+    },
+    card: {
+        maxWidth: 345,
+    },
+    media: {
+        height: 140,
     },
 });
 
+function getSteps() {
+    return ['Detail Confirmation', 'Delivery Information', 'Payment Select'];
+}
+
+function getStepContent(step) {
+    switch (step) {
+        case 0:
+            return (<DetailConfirm/>);
+        case 1:
+            return (<DeliveryInfor/>);
+        case 2:
+            return (<PaymentSelect/>);
+        default:
+            return 'Unknown step';
+    }
+}
+
 class Payment extends React.Component {
     state = {
-        value: 0,
+        activeStep: 0,
+        completed: new Set(),
+        skipped: new Set(),
     };
 
-    handleChange = (event, value) => {
-        this.setState({ value });
+    totalSteps = () => {
+        return getSteps().length;
     };
+
+    isStepOptional = step => {
+        return step === 1;
+    };
+
+    handleNext = () => {
+        let activeStep;
+
+        if (this.isLastStep() && !this.allStepsCompleted()) {
+            // It's the last step, but not all steps have been completed
+            // find the first step that has been completed
+            const steps = getSteps();
+            activeStep = steps.findIndex((step, i) => !this.state.completed.has(i));
+        } else {
+            activeStep = this.state.activeStep + 1;
+        }
+        this.setState({
+            activeStep,
+        });
+    };
+
+    handleBack = () => {
+        this.setState(state => ({
+            activeStep: state.activeStep - 1,
+        }));
+    };
+
+    handleStep = step => () => {
+        this.setState({
+            activeStep: step,
+        });
+    };
+
+    handleComplete = () => {
+        // eslint-disable-next-line react/no-access-state-in-setstate
+        const completed = new Set(this.state.completed);
+        completed.add(this.state.activeStep);
+        this.setState({
+            completed,
+        });
+
+        /**
+         * Sigh... it would be much nicer to replace the following if conditional with
+         * `if (!this.allStepsComplete())` however state is not set when we do this,
+         * thus we have to resort to not being very DRY.
+         */
+        if (completed.size !== this.totalSteps() - this.skippedSteps()) {
+            this.handleNext();
+        }
+    };
+
+    skippedSteps() {
+        return this.state.skipped.size;
+    }
+
+    isStepSkipped(step) {
+        return this.state.skipped.has(step);
+    }
+
+    isStepComplete(step) {
+        return this.state.completed.has(step);
+    }
+
+    completedSteps() {
+        return this.state.completed.size;
+    }
+
+    allStepsCompleted() {
+        return this.completedSteps() === this.totalSteps() - this.skippedSteps();
+    }
+
+    isLastStep() {
+        return this.state.activeStep === this.totalSteps() - 1;
+    }
 
     render() {
         const { classes } = this.props;
-        const { value } = this.state;
+        const steps = getSteps();
+        const { activeStep } = this.state;
 
         return (
             <div className={classes.root}>
-                <AppBar position="static">
-                    <Tabs value={value} onChange={this.handleChange}>
-                        <Tab label="Item One" />
-                        <Tab label="Item Two" />
-                        <Tab label="Item Three" href="#basic-tabs" />
-                    </Tabs>
-                </AppBar>
-                {value === 0 && <TabContainer><DetailConfirm/></TabContainer>}
-                {value === 1 && <TabContainer><DeliveryInfor/></TabContainer>}
-                {value === 2 && <TabContainer><PaymentConfirm/></TabContainer>}
+                <Stepper alternativeLabel nonLinear activeStep={activeStep}>
+                    {steps.map((label, index) => {
+                        const props = {};
+                        const buttonProps = {};
+                        if (this.isStepOptional(index)) {
+                            buttonProps.optional = <Typography variant="caption">Optional</Typography>;
+                        }
+                        if (this.isStepSkipped(index)) {
+                            props.completed = false;
+                        }
+                        return (
+                            <Step key={label} {...props}>
+                                <StepButton
+                                    onClick={this.handleStep(index)}
+                                    completed={this.isStepComplete(index)}
+                                    {...buttonProps}
+                                >
+                                    {label}
+                                </StepButton>
+                            </Step>
+                        );
+                    })}
+                </Stepper>
+                <div>
+                    {this.allStepsCompleted() ? (
+                        <div style={{paddingLeft:'40%'}}>
+                            <Typography className={classes.instructions}>
+                                <Card className={classes.card}>
+                                    <CardMedia
+                                        className={classes.media}
+                                        image="http://localhost:3000/image/success.png"
+                                        title="Success"
+                                    />
+                                    <CardContent>
+                                        <Typography gutterBottom variant="headline" component="h2">
+                                            Success
+                                        </Typography>
+                                    </CardContent>
+                                    <CardActions>
+                                        <Button size="small" color="primary" href={"/account"}>
+                                            Back to Account
+                                        </Button>
+                                    </CardActions>
+                                </Card>
+                            </Typography>
+                        </div>
+                    ) : (
+                        <div>
+                            <Typography className={classes.instructions}>{getStepContent(activeStep)}</Typography>
+                            <div>
+                                <Button
+                                    disabled={activeStep === 0}
+                                    onClick={this.handleBack}
+                                    className={classes.button}
+                                >
+                                    Back
+                                </Button>
+                                <Button
+                                    variant="contained"
+                                    color="primary"
+                                    onClick={this.handleNext}
+                                    className={classes.button}
+                                >
+                                    Next
+                                </Button>
+                                {this.isStepOptional(activeStep) &&
+                                !this.state.completed.has(this.state.activeStep)}
+                                {activeStep !== steps.length &&
+                                (this.state.completed.has(this.state.activeStep) ? (
+                                    <Typography variant="caption" className={classes.completed}>
+                                        Step {activeStep + 1} already completed
+                                    </Typography>
+                                ) : (
+                                    <Button variant="contained" color="primary" onClick={this.handleComplete}>
+                                        {this.completedSteps() === this.totalSteps() - 1 ? 'Finish' :'Complete Step'}
+                                    </Button>
+                                ))}
+                            </div>
+                        </div>
+                    )}
+                </div>
             </div>
         );
     }
 }
 
 Payment.propTypes = {
-    classes: PropTypes.object.isRequired,
+    classes: PropTypes.object,
 };
 
 export default withStyles(styles)(Payment);
