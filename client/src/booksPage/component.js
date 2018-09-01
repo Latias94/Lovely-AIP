c7028de743682874822f42477381a6e01c066981
Yupeili <474680647@qq.com>
2018-09-01 09:46:57 +0800

    make books show

--- client/src/booksPage/component.js
+++ client/src/booksPage/component.js
@@ -8,7 +8,7 @@ import * as style from './booksPageCss';
 // import PopularBooks from '../welcomePage/popularBooks';
 
 
-const totalPrice = (unitPrice, number) => (unitPrice * number);
+const totalPrice = (unitPrice, number) => (unitPrice * number).toFixed(2);
 const isInstock = number => (number ? 'In Stock.' : 'Out of Stock');
 const menu = (
 	<Menu>
@@ -50,11 +50,11 @@ const booksPageComponent = props => (
 				</div>
 			</div>
 			<div style={style.rightPart}>
-				<div style={{ width: '70%', marginLeft: '15%' }}>
+				<div style={{ width: '76%', marginLeft: '12%' }}>
 					<div>
 						<span style={{ marginRight: '15px' }}>Buy</span>
 						<InputNumber min={1} max={10} value={props.bookSelectNumber} onChange={value => props.onbookNumberChange(value)} />
-						<span style={{ marginLeft: '15px' }}>{`${totalPrice(props.bookSelectNumber, props.bookPrice)}$`}</span>
+						<span style={{ marginLeft: '15px' }}>{`$${totalPrice(props.bookSelectNumber, props.bookPrice)}`}</span>
 					</div>
 					<h4 style={{ marginTop: '15px' }}>{isInstock(props.stockNumber)}</h4>
 					<span>Deliver to Australia</span>
