4c5f27823abeca776ecf1aeb773c0d5b55a316c8
Steven Ding <anluoridge@gmail.com>
2018-09-10 19:03:58 +1000

    remove unused import

--- client/src/account/components/Account/index.jsx
+++ client/src/account/components/Account/index.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import {withStyles} from '@material-ui/core/styles';
 import Modal from 'react-responsive-modal';
 import AccountTab from './AccountTab'
