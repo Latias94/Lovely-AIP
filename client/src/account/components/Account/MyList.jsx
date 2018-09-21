c7549e21a8d0a9cd33cd3bb23c0efabc994b8eff
jennyhu19900914@gmail.com <jennyhu19900914@gmail.com>
2018-09-21 12:58:14 +1000

    Change UI of Add Newbook

--- client/src/account/components/Account/MyList.jsx
+++ client/src/account/components/Account/MyList.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
-import green from '@material-ui/core/colors/green';
 import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
