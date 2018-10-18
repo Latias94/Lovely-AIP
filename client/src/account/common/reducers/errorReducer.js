b8c2428bdd6b123aeb0e0362b23945ddb1cc814c
Steven Ding <anluoridge@gmail.com>
2018-10-18 21:49:10 +1100

    Fix showing of email existed and user not found.

--- client/src/account/common/reducers/errorReducer.js
+++ client/src/account/common/reducers/errorReducer.js
@@ -5,7 +5,12 @@ const initialState = {};
 export default function (state = initialState, action) {
 	switch (action.type) {
 	case GET_ERRORS:
-		// use the key value in the payload directly
+		if(action.payload.hasOwnProperty('emailexist')) {
+			return { email: 'This email address is already singed up.' };
+		}
+		if(action.payload.hasOwnProperty('usernotfound')) {
+			return { email: 'The email address is not signed up.' };
+		}
 		return action.payload;
 	default:
 		return state;
